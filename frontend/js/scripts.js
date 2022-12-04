7800ad6a04e1ee27a7b4a51f434d699e296dc762
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-04 05:20:55 +0200

    Updating <> Frontend scripts.js

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -15,3 +15,25 @@ timebank_pages.loaderFunction = (func_name) => {
 // #########
 // Functions
 // #########
+
+timebank_pages.loadLoginSignup = () => {
+    // Retrieving the login input fields & button
+    const login_email = document.getElementById('logemail')
+    const login_password = document.getElementById('logpass')
+    const login_btn = document.getElementById('logbtn')
+
+    // Retrieving the signup input fields & button
+    const signup_username = document.getElementById('signname')
+    const signup_email = document.getElementById('signemail')
+    const signup_password = document.getElementById('signpass')
+    const signup_btn = document.getElementById('signbtn')
+
+    login_btn.addEventListener('click', () => {
+        console.log('Login button clicked')
+    })
+
+    signup_btn.addEventListener('click', () => {
+        console.log('Signup button clicked')
+    })
+
+}
