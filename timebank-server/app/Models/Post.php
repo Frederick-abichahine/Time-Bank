4a8476e903de4cbe0fce083b1a319f3f15801951
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-05 23:01:28 +0200

    Updating <> Backend Post Model

--- timebank-server/app/Models/Post.php
+++ timebank-server/app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+    protected $fillable = [
+        'skill_to_offer',
+        'skill_to_learn',
+        'offer_time',
+        'description',
+        'user_id',
+    ];
 }
