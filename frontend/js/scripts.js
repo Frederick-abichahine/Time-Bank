af87f17cc3ff5fdd96ca6a219bd8254123313b19
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-05 04:38:56 +0200

    Updating <> Frontend scripts.js

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -48,6 +48,8 @@ timebank_pages.loadLoginSignup = () => {
 }
 
 timebank_pages.loadIndex = () => {
+
+    // Parallax Effect
     const banner = document.getElementById('banner')
     const moon = document.getElementById('moon')
     const mountain = document.getElementById('mountain')
@@ -60,4 +62,41 @@ timebank_pages.loadIndex = () => {
         mountain.style.top = -value * 0.25 + 'px'
         title.style.top = value * 0.25 + 'px'
     })
+
+    // Navigation Bar With Spotlight Effect
+    const uls = document.querySelectorAll("nav ul");
+    const links = [...document.querySelectorAll("nav a")];
+    const light = document.querySelector("nav .tubelight");
+    let activeIndex = 0;
+    let currentIndex = 0;
+    let increment = 1;
+    links.forEach((link, index) => 
+    {
+        if (links[index].classList.contains("active"))
+        {
+            light.style.left = `${links[index].offsetLeft + light.offsetWidth / 4}px`;
+        }
+        link.addEventListener("click", (e) => 
+        {
+            activeIndex = index;
+            let t = setInterval(() => 
+            {
+                if (activeIndex > currentIndex) increment = 1;
+                else if (activeIndex < currentIndex) increment = -1;
+                currentIndex += increment;
+
+                links[currentIndex].classList.add("active");
+                if (currentIndex != -1)
+                    links[currentIndex - increment].classList.remove("active");
+
+                if (currentIndex == activeIndex) 
+                {
+                    e.target.classList.add("active");
+                    increment = 0;
+                    clearInterval(t);
+                }
+            }, 50);
+            light.style.left = `${e.target.offsetLeft + light.offsetWidth / 4}px`;
+        });
+    });
 }
