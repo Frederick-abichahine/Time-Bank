3abf4f7797de7fbc314727ee9eba99b6c460744c
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-12 23:45:36 +0200

    Creating <> Backend Migrations / DB Tables

--- timebank-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ timebank-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('jwt_token');
+            $table->string('location');
+            $table->string('profile_picture');
+            $table->string('description');
+            $table->integer('user_type_id'); //->default(1)
             $table->timestamps();
         });
     }
