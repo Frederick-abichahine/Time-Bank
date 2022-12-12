3cc3ed2d18e2c4cdd96b64b861cc4b6cfc79ad31
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-12 23:46:24 +0200

    Creating <> Backend Controllers

--- timebank-server/app/Http/Controllers/User/UserController.php
+++ timebank-server/app/Http/Controllers/User/UserController.php
@@ -4,19 +4,44 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
 
 class UserController extends Controller
 {
     function login(Request $request){
-        return response()->json([
-            'message' => 'Login successful Post',
-            'data' => $request->all()
+
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
         ]);
-    }
-    function login2(Request $request){
-        return response()->json([
-            'message' => 'Login successful Get',
-            'data' => $request->all()
-        ]);
+
+        if($validate->fails()){
+            return response()->json([
+                "status" => "failed",
+                "results" => []
+            ]);
+        } else{
+            //find if user exists
+            $user = User::where('email', $request->email)
+                       -> where('password', $request->password)
+                       -> get();
+
+            if(!$user){
+                return response()->json([
+                    "status" => "user not found",
+                    "results" => []
+                ]);
+            }else {
+                return response()->json([
+                    "status" => "success",
+                    "results" => $user
+                ]);
+            }
+
+        }
+        // return response()->json([
+        //     'message' => 'Login successful Post',
+        //     'data' => $request->all()
+        // ]);
     }
 }
