ca0283ebf7988afa84f7e1f6def91d4c3457d4f6
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-13 02:18:42 +0200

    Updating <> Backend UserController

--- timebank-server/app/Http/Controllers/User/UserController.php
+++ timebank-server/app/Http/Controllers/User/UserController.php
@@ -4,44 +4,46 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 use Validator;
 
 class UserController extends Controller
 {
     function login(Request $request){
 
+        // to check if the request is valid, whether the email and password are provided
         $validate = Validator::make($request->all(), [
             'email' => 'required',
             'password' => 'required',
         ]);
 
+        // if the request is not valid, meaning the email or password are not provided
         if($validate->fails()){
             return response()->json([
                 "status" => "failed",
                 "results" => []
             ]);
-        } else{
-            //find if user exists
+        } 
+        // if the request is valid, meaning the email and password are provided
+        else{
+            // to find if user exists
             $user = User::where('email', $request->email)
-                       -> where('password', $request->password)
-                       -> get();
-
-            if(!$user){
+            ->where('password', $request->password)
+            ->get();
+            // if user does not exist
+            if($user == '[]'){
                 return response()->json([
                     "status" => "user not found",
                     "results" => []
                 ]);
-            }else {
+            }
+            // if user exists
+            else {
                 return response()->json([
                     "status" => "success",
                     "results" => $user
                 ]);
             }
-
         }
-        // return response()->json([
-        //     'message' => 'Login successful Post',
-        //     'data' => $request->all()
-        // ]);
     }
 }
