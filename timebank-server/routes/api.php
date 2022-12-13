765f31f98cb5bb71f06409cfc7ec03b3e9b0b826
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-13 03:17:17 +0200

    Updating <> Backend Routes

--- timebank-server/routes/api.php
+++ timebank-server/routes/api.php
@@ -11,4 +11,7 @@ use App\Http\Controllers\User\UserController;
 Route::group(['prefix' => 'v0.1'], function () {
     Route::post('login', [UserController::class, 'login']);
     Route::post('signup', [UserController::class, 'signup']);
+    Route::get('getAllPosts', [PostController::class, 'getAllPosts']); //to display all posts on index page
+    Route::get('getMyPosts', [PostController::class, 'getMyPosts']); //to display all my posts on my profile page
+    Route::get('getSpecificPosts', [PostController::class, 'getSpecificPosts']); //to display specific posts based on search on index page (search by skil to learn)
 });
