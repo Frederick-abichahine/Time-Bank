e34115a4a7e22d3f6943c6a2bb2d3a31f70fad02
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-17 22:55:15 +0200

    Updating <> Backend JWT

--- timebank-server/routes/api.php
+++ timebank-server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\User\PostController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
