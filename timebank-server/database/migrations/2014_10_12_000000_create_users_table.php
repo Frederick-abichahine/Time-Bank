4485abcbf769276259433f127b2bb4013ad3b0cf
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-19 02:07:42 +0200

    Fixing <> Backend User Model & Migration

--- timebank-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ timebank-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('jwt_token');
-            $table->string('location');
-            $table->string('profile_picture');
-            $table->string('description');
+            $table->string('jwt_token')->default(''); //when a user is created they will have an empty jwt token, it is updated as the user logs in, and destroyed when logged out
+            $table->string('location')->default(''); //to be fixed later, take location from google when a user registers
+            $table->string('profile_picture')->default(''); //displays a 'default user image', to be updated later
+            $table->string('description')->default(''); //empty till the user edits their profile
             $table->integer('user_type_id')->default('1'); //to set all new users by default as normal users
             $table->timestamps();
         });
