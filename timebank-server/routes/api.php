f503169d6389c33325ea42bee6050fdfdfea5d00
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-12-20 00:15:12 +0200

    Updating <> Backend Routes

--- timebank-server/routes/api.php
+++ timebank-server/routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\User\PostController;
+use App\Http\Controllers\User\UserController;
 
+// Grouping all routes related to version 0.1
 Route::group(['prefix' => 'v0.1'], function () {
+
+    // Grouping all routes that require authentication & authorization
     Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
         Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
@@ -13,5 +17,15 @@ Route::group(['prefix' => 'v0.1'], function () {
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::get('user-profile', [AuthController::class, 'userProfile']); 
     });
-    Route::get('count-posts/{id?}', [PostController::class, 'countPosts']);
+    
+    // Grouping all routes related to posts
+    Route::group(['prefix' => 'posts'], function () {
+        Route::get('count-posts', [PostController::class, 'countPosts']);
+        Route::get('get-all-posts', [PostController::class, 'getAllPosts']);
+    });
+
+    // Grouping all routes related to users
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('get-user-details/{id?}', [UserController::class, 'getUserDetails']);
+    });
 });
